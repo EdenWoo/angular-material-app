09b3bb030d73ae2819ac04a3ccd02805c7ff6825
EdenWoo <xiaoyumqyz@gmail.com>
2022-12-21 00:08:03 +1300

    app component

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AppComponent} from './app/app.component';
 import {RouterOutlet} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     declarations: [AppComponent],
     exports: [AppComponent],
     imports: [
         CommonModule,
-        RouterOutlet
+        RouterOutlet,
+        MatButtonModule
     ]
 })
 export class CoreModule {
